0f147e76c87fcb769c6478bb06a50f0dcaad4a83
Nicholas Laustrup <124007393+nicklaustrup@users.noreply.github.com>
2026-03-25 15:13:46 -0700

    completed step 1

--- dataframes.py
+++ dataframes.py
@@ -1,3 +1,36 @@
 # Author: Nick Laustrup
 # Date: 3/25/2026
 # PySpark DataFrames
+from pyspark.sql import SparkSession
+
+def main():
+    spark = SparkSession.builder.appName("DataFrames").getOrCreate()
+    
+    # ----------------------------------- #
+    # Step 1: Load and Explore Data
+    # ----------------------------------- #
+    print("-"*5, "Step 1", "-"*5)
+    # Load data
+    df = spark.read.csv("people.csv", header=True, inferSchema=True)
+    print("=" * 100)
+    
+    # Show first few rows
+    print("Printing first few rows: ")
+    df.show(n=5)
+    
+    # Print schema
+    print("Printing schema: ")
+    df.printSchema()
+    
+    # Count how many records
+    print("Number of records in dataset: ", df.count())
+    
+    # ----------------------------------- #
+    # Step 2: Filtering and Selecting Columns
+    # ----------------------------------- #
+    print("-"*5, "Step 1", "-"*5)
+    
+    spark.stop()
+
+if __name__ == '__main__':
+    main()
