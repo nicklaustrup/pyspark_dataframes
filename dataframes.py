29341fd1167bc736dd08ba7fe457e36f1645bfaa
Nicholas Laustrup <124007393+nicklaustrup@users.noreply.github.com>
2026-03-25 16:23:24 -0700

    completed step 4

--- dataframes.py
+++ dataframes.py
@@ -1,11 +1,11 @@
 # Author: Nick Laustrup
 # Date: 3/25/2026
 # PySpark DataFrames
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, functions as F
 
 def main():
+    print("=" * 100)
     spark = SparkSession.builder.appName("DataFrames").getOrCreate()
-    
     # ----------------------------------- #
     # Step 1: Load and Explore Data
     # ----------------------------------- #
@@ -13,7 +13,6 @@ def main():
     
     # Load data
     df = spark.read.csv("people.csv", header=True, inferSchema=True)
-    print("=" * 100)
     
     # Show first few rows
     print("Printing first few rows: ")
@@ -45,7 +44,37 @@ def main():
     print("Average salary from NY: ")
     df_ny.agg({"salary": "avg"}).show()
 
+    # ----------------------------------- #
+    # Step 3: Grouping and Aggregation
+    # ----------------------------------- #
+    print("-"*5, "Step 3", "-"*5)
     
+    # Group the data by city and calculate the average salary per city
+    print("Group by city & Sort by Avg Salary: ")
+    df_city = df.groupBy("city").avg("salary").sort("avg(salary)", ascending=False)
+    df_city.show()
+    
+    # ----------------------------------- #
+    # Step 4: DataFrame Transformation
+    # ----------------------------------- #
+    print("-"*5, "Step 4", "-"*5)
+        
+    # Add new column: income bracket based on salary
+    df_with_bracket = df.withColumn(
+        "income_bracket",
+        F.when(F.col("salary") < 80000, "Low")
+        .when(F.col("salary") < 100000, "Mid")
+        .otherwise("High")
+    )
+    
+    print("Add income bracket: ")
+    df_with_bracket.show()
+
+    print("Number of people in each income bracket: ")
+    df_income_count = df_with_bracket.groupBy(df_with_bracket.income_bracket).count()
+    df_income_count.show()
+    
+    print("=" * 100)
     spark.stop()
 
 if __name__ == '__main__':
